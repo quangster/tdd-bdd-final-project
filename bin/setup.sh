cc2259058807f68e093bca9a8c4d863cfb4557d4
quangster <buiduyquang2k4@gmail.com>
2024-09-20 05:03:23 +0000

    Fix script

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,11 +5,11 @@ echo "**************************************************"
 
 echo "*** Installing Python 3.9 and Virtual Environment"
 sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python3.9 python3.9-venv
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python3.8-venv
 
-echo "*** Making Python 3.9 the default..."
-sudo update-alternatives --remove-all python3
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 1
+# echo "*** Making Python 3.9 the default..."
+# sudo update-alternatives --remove-all python3
+# sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 1
 
 echo "*** Checking the Python version..."
 python3 --version
