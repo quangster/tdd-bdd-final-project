9cf427e1a51970c03fc8254ca5ebbc3866a36a32
quangster <buiduyquang2k4@gmail.com>
2024-09-20 07:48:38 +0000

    Implementing Steps

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -51,7 +51,7 @@ def step_impl(context):
             "name": row["name"],
             "description": row["description"],
             "price": row["price"],
-            "available": row["available"],
+            "available": row["available"] in ["True", "true", 1],
             "category": row["category"],
         }
         context.resp = requests.post(rest_endpoint, json=payload)
